05a74d0f8386494aa55c383ac9870d1b3a760cb4
wasuken <wevorence@gmail.com>
2019-03-26 16:50:03 +0900

    update impl.

--- app/controllers/journals_controller.rb
+++ app/controllers/journals_controller.rb
@@ -1,5 +1,6 @@
 class JournalsController < ApplicationController
-  before_action :authenticate_user!, only: :index
+  before_action :authenticate_user!,
+                only: [:index, :create, :update_page, :update]
   def index
     p @user_id
     @journals = Journal.where('user_id = ?', current_user.id)
@@ -12,6 +13,17 @@ class JournalsController < ApplicationController
                    kind: params['kind']['name'],
                    user_id: params['journal']['user_id'],
                    usefulness: params['journal']['usefulness'])
+    redirect_to '/'
+  end
+  def update_page
+    @journal = Journal.where('id = ?', params[:id]).all.first
+    render 'journals/update_page'
+  end
+  def update
+    Journal.find(params['journal']['id'])
+      .update(amount: params['journal']['amount'],
+              kind: params['kind']['name'],
+              usefulness: params['journal']['usefulness'])
     redirect_to '/'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'journals#index'
   get '/users/sign_out', to: 'devise/sessions#destroy'
+  get '/journal/update', to: 'journals#update_page'
+  patch '/journal', to: 'journals#update'
   post '/journal', to: 'journals#create'
 end
